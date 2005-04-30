73eda3343de55b69120959fc5bd22876e6666f66
Marcus Ramberg <mramberg@cpan.org>
2005-04-30 21:20:29 +0000

    Import of MRAMBERG/Catalyst-Plugin-Unicode-0.2 from CPAN.
    gitpan-cpan-distribution: Catalyst-Plugin-Unicode
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         MRAMBERG/Catalyst-Plugin-Unicode-0.2.tar.gz
    gitpan-cpan-author:       MRAMBERG
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Plugin/Unicode.pm
+++ lib/Catalyst/Plugin/Unicode.pm
@@ -2,14 +2,24 @@ package Catalyst::Plugin::Unicode;
 
 use strict;
 
-our $VERSION = '0.1';
+our $VERSION = '0.2';
 
 sub finalize {
     my $c = shift;
 
-    if ( $c->response->body && $c->response->content_type =~ /^text/ ) {
-        utf8::encode( $c->response->{body} );
+    unless ( $c->response->body ) {
+        return $c->NEXT::finalize;
     }
+
+    unless ( $c->response->content_type =~ /^text/ ) {
+        return $c->NEXT::finalize;
+    }
+
+    unless ( utf8::is_utf8( $c->response->body ) ) {
+        return $c->NEXT::finalize;
+    }
+
+    utf8::encode( $c->response->{body} );
 
     $c->NEXT::finalize;
 }
@@ -47,18 +57,18 @@ Catalyst::Plugin::Unicode - Unicode aware Catalyst
 On request, decodes all params from UTF-8 octets into a sequence of 
 logical characters. On response, encodes body into UTF-8 octets.
 
-=head1 OVERRIDDEN METHODS
+=head1 OVERLOADED METHODS
 
 =over 4
 
 =item finalize
 
-Encodes body as UTF-8 octets.
+Encodes body into UTF-8 octets.
 
 =item prepare_parameters
 
-Encode parameters as UTF-8 octets.
- 
+Decodes parameters into a sequence of logical characters.
+
 =back
 
 =head1 SEE ALSO
